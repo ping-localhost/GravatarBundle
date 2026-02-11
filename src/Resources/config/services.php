f48f11a5d743d3e30ec345db45a8a291eea86013
Mitchell <37925797+ping-localhost@users.noreply.github.com>
2026-02-11 22:56:57 +0000

    Try and fix services.php

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
 
-    $services->set('gravatar.api', Pyrrah\GravatarBundle\GravatarApi::class)
+    $services->set('gravatar.api', \Pyrrah\GravatarBundle\GravatarApi::class)
         ->args([
             null, // will be filled in with size dynamically
             null, // will be filled in with rating dynamically
@@ -15,12 +15,13 @@ return static function (ContainerConfigurator $container): void {
             null, // will be filled in with format dynamically
         ]);
 
-        $services->set('templating.helper.gravatar', Pyrrah\GravatarBundle\Templating\Helper\GravatarHelper::class)
+        $services->set('templating.helper.gravatar', \Pyrrah\GravatarBundle\Templating\Helper\GravatarHelper::class)
             ->args([
-                service('gravatar.api')
+                service('gravatar.api'),
+                service('router')
             ]);
 
-    $services->set('twig.extension.gravatar', Pyrrah\GravatarBundle\Twig\GravatarExtension::class)
+    $services->set('twig.extension.gravatar', \Pyrrah\GravatarBundle\Twig\GravatarExtension::class)
         ->tag('twig.extension', ['alias' => 'gravatar'])
         ->args([
             service('templating.helper.gravatar')
